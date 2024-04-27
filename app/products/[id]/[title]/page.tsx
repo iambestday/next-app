7f84dcb7d613c69fc5028a1c5fc39522a41caec7
iambestday <behrouz.asghari@gmail.com>
2024-04-27 14:47:37 +0330

    Add role for redirect to correct route

---
+++ app/products/[id]/[title]/page.tsx
@@ -0,0 +1,69 @@
+import { Card, CardContent, CardMedia, Rating, Typography } from "@mui/material";
+import { redirect } from "next/navigation";
+import "./Product.scss";
+
+interface ProductProps {
+  params: {
+    id: string;
+    title: string;
+  };
+}
+
+const getData = async (id: string): Promise<Product> => {
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+    next: { revalidate: 3600 },
+  });
+  return response.json();
+};
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+const Product = async ({ params }: ProductProps) => {
+  const product = await getData(params.id);
+  let newText: string = product.title.replace(/[.,\-_]/g, '').replace(/\s+/g, '')
+
+  if (newText !== params.title) {
+    redirect(`/products/${params.id}/${newText}`);
+  } else {
+    return (
+      <div className="centered-card">
+        <Card className="custom-card">
+          <CardMedia component="img" height="200" image={product.image} alt={product.title} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {product.title}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              {product.category}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {product.description}
+            </Typography>
+            <Typography variant="h6" color="text.primary">
+              ${product.price}
+            </Typography>
+            <div className="rating-container">
+              <Rating name="read-only" value={product.rating.rate} readOnly />
+              <Typography variant="body2" color="text.secondary">
+                {product.rating.count} : count of rate
+              </Typography>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+};
+
+export default Product;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -9,13 +9,14 @@ const getData = async () => {
 };
 
 const Products = async () => {
-    const products = await getData();
-    return (
-        <div style={{ display: "flex", justifyContent: "center", padding: "20px" }}>
+  const products = await getData();
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", padding: "20px" }}>
       <Grid container spacing={3} style={{ maxWidth: "1200px" }}>
         {products.map((product: any, index: number) => (
           <Grid item key={product.id} xs={12} sm={6} md={3}>
-            <Link href={`/products/${product.id}`} key={index}>
+<Link href={`/products/${product.id}/${product.title.replace(/[.,\-_]/g, '').replace(/\s+/g, '')}`} key={index}>
               <Card style={{ height: "100%" }}>
                 <CardMedia
                   component="img"
@@ -30,21 +31,19 @@ const Products = async () => {
                   <Typography variant="body2" color="text.secondary">
                     {product.description}
                   </Typography>
-               
                 </CardContent>
                 <CardActions>
-                     <Typography variant="h6" color="text.primary">
+                  <Typography variant="h6" color="text.primary">
                     ${product.price}
                   </Typography>
-      </CardActions>
+                </CardActions>
               </Card>
             </Link>
           </Grid>
         ))}
       </Grid>
     </div>
-    );
-}
+  );
+};
 
 export default Products;
-
