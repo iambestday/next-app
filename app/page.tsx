f162bafe271de043f8f2142b154fa88c687582f5
iambestday <behrouz.asghari@gmail.com>
2024-04-27 14:47:02 +0330

    Add role for redirect to correct route

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,12 @@
-import Image from "next/image";
-import { Grid, Card, CardContent, CardMedia, Typography } from "@mui/material";
-import Link from "next/link";
+import Products from "./products/page";
 
-const getData = async () => {
-  const data = await fetch("https://fakestoreapi.com/products", {
-    next: { revalidate: 3600 },
-  });
-  return data.json();
-};
 
 const Home = async () => {
-  const products = await getData();
 
   return (
-    <div>
-      <Grid container spacing={3}>
-        {products.map((product: any,index:number) => (
-          <Grid item key={product.id} xs={12} sm={6} md={3}>
-            <Link
-            href={`/products/${product.id}`}
-            key={index}
-            >
-            <Card >
-              <CardMedia
-                component="img"
-                height="200"
-                image={product.image}
-                alt={product.title}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {product.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {product.description}
-                </Typography>
-                <Typography variant="h6" color="text.primary">
-                  ${product.price}
-                </Typography>
-              </CardContent>
-            </Card>
-            </Link>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
+  <div>
+    <Products/>
+  </div>
   );
 };
 
---
+++ app/products/page.tsx
@@ -0,0 +1,50 @@
+import { Grid, Card, CardContent, CardMedia, Typography, CardActions } from "@mui/material";
+import Link from "next/link";
+
+const getData = async () => {
+  const data = await fetch("https://fakestoreapi.com/products", {
+    next: { revalidate: 3600 },
+  });
+  return data.json();
+};
+
+const Products = async () => {
+    const products = await getData();
+    return (
+        <div style={{ display: "flex", justifyContent: "center", padding: "20px" }}>
+      <Grid container spacing={3} style={{ maxWidth: "1200px" }}>
+        {products.map((product: any, index: number) => (
+          <Grid item key={product.id} xs={12} sm={6} md={3}>
+            <Link href={`/products/${product.id}`} key={index}>
+              <Card style={{ height: "100%" }}>
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={product.image}
+                  alt={product.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {product.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {product.description}
+                  </Typography>
+               
+                </CardContent>
+                <CardActions>
+                     <Typography variant="h6" color="text.primary">
+                    ${product.price}
+                  </Typography>
+      </CardActions>
+              </Card>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+    );
+}
+
+export default Products;
+
